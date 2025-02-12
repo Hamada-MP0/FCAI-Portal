0503e09aa273dab9ab2cd5842a85fc3e5f43bc76
Enf-Mohamed <engmp889@gmail.com>
2025-02-12 23:42:00 +0200

    The startof the project FCAI Portal

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import DarkMode from './component/dark&light/DarkMode'
+function App() {
+
+
+  return (
+    <>
+    <div className="text-3xl font-bold bg-red-500 dark:bg-white dark:text-black">
+      <DarkMode/>
+      ddddd
+    </div>
+    </>
+  )
+}
+
+export default App
