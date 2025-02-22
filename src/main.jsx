3c421280840ff10460798f0e9bb58a05006ad964
Enf-Mohamed <engmp889@gmail.com>
2025-02-22 20:00:10 +0200

    Resolve merge conflicts

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,18 @@ import App from './App.jsx'
 import "./i18n"; // استيراد إعدادات الترجمة
 import "./index.css";
 
+import { registerSW } from 'virtual:pwa-register';
 
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New version available. Reload?")) {
+      updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    console.log("App is ready to work offline.");
+  },
+});
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
